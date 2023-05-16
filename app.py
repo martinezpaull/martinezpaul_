9cc92f9a3039679f9da2cf8757f801d1fe458e32
martinezpaull <133735768+martinezpaull@users.noreply.github.com>
2023-05-16 12:55:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
         image = ImageOps.fit(image_data,size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
-        img_reshape = np.reshape(image, (28, 28, 1))
+        img_reshape = np.reshape(image, (1, 28, 28, 1))
         prediction = model.predict(img_reshape)
         return prediction
 
